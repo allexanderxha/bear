dca71215e027b7299c2f79240d57e6ca62efca8a
allexanderbergmns <socials@libregrad.pp.ua>
2026-08-24 14:54:02 +0200

    core

---
+++ main.v
@@ -0,0 +1,46 @@
+// vuurraaf/v
+// A complete toolchain for VuurRaaf, in V. This toolchain is made from zero,
+// and includes custom compiler, assembler, linker, and runtime. 
+// It is designed to be simple and easy to understand, while still being powerful 
+// enough to compile and run VuurRaaf programs.
+
+module main
+
+import os
+import v.vmod
+
+fn toolchain_loader() {}
+
+fn toolchain_unloader() {}
+
+fn toolchain_alloc() {}
+
+fn toolchain_free() {}
+
+fn toolchain_compile() {}
+
+fn toolchain_assemble() {}
+
+fn toolchain_link() {}
+
+fn toolchain_run() {}
+
+fn toolchain_debug() {}
+
+fn toolchain_help() {}
+
+fn toolchain_version() {}
+
+fn toolchain_info() {}
+
+fn toolchain_config() {}
+
+fn toolchain_test() {}
+
+fn toolchain_bench() {}
+
+fn toolchain_clean() {}
+
+fn toolchain_up() {}
+
+fn toolchain_symlink() {}
